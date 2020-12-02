aba4bb05e3637f8d967f69196ff62251ab00598a
TurtleDonatello <padre.vitaliy@gmail.com>
2020-12-02 20:34:44 +0200

    Reanamete

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,13 +1,13 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React from "react";
-import MyPosts from "./MyPosts/MyPosts";
+import MyPosts from './MyPosts/MyPosts';
 import classes from './Profile.module.css';
 
 const Profile = () => {
   return (
     <div className={classes.content}>
       <div>
-        <img src='https://asiamountains.net/upload/slide/slide-1960x857-07.jpg' />
+          <img src='https://asiamountains.net/upload/slide/slide-1960x857-07.jpg'></img>
       </div>
       <div>
         Ava+Description
