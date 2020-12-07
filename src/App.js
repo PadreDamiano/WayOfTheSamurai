55c58d9319a497947cfe1308dcff4727b0064d9a
TurtleDonatello <padre.vitaliy@gmail.com>
2020-12-07 13:50:25 +0200

    Upgrayte Dialogs

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import Profile from "./components/Profile/Profile";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Dialogs from "./components/Dialogs/Dialogs";
-import Route, {BrowserRouter} from "react-router-dom";
+import  {BrowserRouter} from "react-router-dom";
+import Route from "react-router-dom/es/Route";
 
 const App = () => {
     return (
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,47 @@
 import React from 'react';
 import classes from './Dialogs.module.css';
 
+
 const Dialogs = () => {
     return (
-        <div>
-            Dialog
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItem}>
+                <div className={classes.dialog}>
+                    Vitaliy
+                </div>
+                <div className={classes.dialog}>
+                    Natalie
+                </div>
+                <div className={classes.dialog}>
+                    Cristi
+                </div>
+                <div className={classes.dialog}>
+                    George
+                </div>
+                <div className={classes.dialog}>
+                    Ann
+                </div>
+                <div className={classes.dialog}>
+                    Fillip
+                </div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>
+                    Hello
+                </div>
+                <div className={classes.message}>
+                    How are you?
+                </div>
+                <div className={classes.message}>
+                    My name is Natalie
+                </div>
+                <div className={classes.message}>
+                    Good day
+                </div>
+                <div className={classes.message}>
+                    Goodbye
+                </div>
+            </div>
         </div>
     )
 }
